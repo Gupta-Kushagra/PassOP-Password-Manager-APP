84254702df9c29e47315a8e9d28e99106768f119
Kushagra Gupta <kushagragupta9793@gmail.com>
2025-01-31 23:38:50 +0530

    Update Manager.jsx

--- frontend/src/components/Manager.jsx
+++ frontend/src/components/Manager.jsx
@@ -2,30 +2,26 @@ import React from 'react'
 import { useRef, useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import { v4 as uuidv4 } from 'uuid';
-
 import 'react-toastify/dist/ReactToastify.css';
 
 const Manager = () => {
-    const backend_uri ="https://ud-5.onrender.com/";
+    const backend_uri = "https://ud-5.onrender.com/";
     const ref = useRef()
     const passwordRef = useRef()
     const [form, setform] = useState({ site: "", username: "", password: "" })
     const [passwordArray, setPasswordArray] = useState([])
 
-   const getPasswords = async() => {
-    let req = await fetch(backend_uri)
-    let passwords = await req.json()
-    if (passwords) {
-        console.log(passwords)
+    const getPasswords = async () => {
+        let req = await fetch(backend_uri)
+        let passwords = await req.json()
         setPasswordArray(passwords)
-    
     }
-   }
+
 
     useEffect(() => {
         getPasswords()
-     
     }, [])
+
 
     const copyText = (text) => {
         toast('Copied to clipboard!', {
@@ -41,8 +37,6 @@ const Manager = () => {
         navigator.clipboard.writeText(text)
     }
 
-
-
     const showPassword = () => {
         passwordRef.current.type = "text"
         console.log(ref.current.src)
@@ -57,44 +51,18 @@ const Manager = () => {
 
     }
 
-    const savePassword = async() => {
-        if(form.site.length >3 && form.username.length >3 &&form.password.length >3){
+    const savePassword = async () => {
+        if (form.site.length > 3 && form.username.length > 3 && form.password.length > 3) {
 
-//if any such id exists int the db, delete it
-         await fetch(backend_uri,{method:"DELETE", headers: {"Content-Type": "application/json"}, body: JSON.stringify({ id: form.id }) })
+            // If any such id exists in the db, delete it 
+            await fetch(backend_uri, { method: "DELETE", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ id: form.id }) })
 
-            setPasswordArray([...passwordArray, {...form, id: uuidv4()}])
-           let res = await fetch(backend_uri,{method:"POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify({...form, id: uuidv4() }) })
-            // localStorage.setItem("passwords", JSON.stringify([...passwordArray, {...form, id: uuidv4()}]))
-           // console.log([...passwordArray, form])
+            setPasswordArray([...passwordArray, { ...form, id: uuidv4() }])
+            await fetch(backend_uri, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ ...form, id: uuidv4() }) })
+
+            // Otherwise clear the form and show toast
             setform({ site: "", username: "", password: "" })
             toast('Password saved!', {
-            position: "top-right",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark",
-        });
-    }
-    else{
-        toast('Error: Password not saved!');
-    }
-
-    }
-
-    const deletePassword = async(id) => {
-        console.log("Deleting password with id ", id)
-        let c = confirm("Do you really want to delete this password?")
-        if(c){
-            setPasswordArray(passwordArray.filter(item=>item.id!==id))
-           
-            let res = await fetch(backend_uri,{method:"DELETE", headers: {"Content-Type": "application/json"}, body: JSON.stringify({ id }) })
-
-            // localStorage.setItem("passwords", JSON.stringify(passwordArray.filter(item=>item.id!==id))) 
-            toast('Password Deleted!', {
                 position: "top-right",
                 autoClose: 5000,
                 hideProgressBar: false,
@@ -105,16 +73,37 @@ const Manager = () => {
                 theme: "dark",
             });
         }
+        else {
+            toast('Error: Password not saved!');
+        }
+
+    }
+
+    const deletePassword = async (id) => {
+        console.log("Deleting password with id ", id)
+        let c = confirm("Do you really want to delete this password?")
+        if (c) {
+            setPasswordArray(passwordArray.filter(item => item.id !== id))
             
+            await fetch(backend_uri, { method: "DELETE", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ id }) })
+
+            toast('Password Deleted!', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true, 
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+        }
+
     }
+
     const editPassword = (id) => {
-         
-        console.log("Editing password with id ", id)
-        setform({...passwordArray.filter(i=>i.id===id)[0], id : id}) 
-        setPasswordArray(passwordArray.filter(item=>item.id!==id)) 
-
+        setform({ ...passwordArray.filter(i => i.id === id)[0], id: id })
+        setPasswordArray(passwordArray.filter(item => item.id !== id))
     }
-
 
 
     const handleChange = (e) => {
@@ -122,23 +111,8 @@ const Manager = () => {
     }
 
 
-
     return (
         <>
-            <ToastContainer
-                position="top-right"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-                transition="Bounce"
-            />
-            {/* Same as */}
             <ToastContainer />
             <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
             <div className=" p-3 md:mycontainer min-h-[88.2vh] ">
@@ -155,7 +129,6 @@ const Manager = () => {
                     <div className="flex flex-col md:flex-row w-full justify-between gap-8">
                         <input value={form.username} onChange={handleChange} placeholder='Enter Username' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="username" id="username" />
                         <div className="relative">
-
                             <input ref={passwordRef} value={form.password} onChange={handleChange} placeholder='Enter Password' className='rounded-full border border-green-500 w-full p-4 py-1' type="password" name="password" id="password" />
                             <span className='absolute right-[3px] top-[4px] cursor-pointer' onClick={showPassword}>
                                 <img ref={ref} className='p-1' width={26} src="icons/eye.png" alt="eye" />
@@ -212,7 +185,7 @@ const Manager = () => {
                                     </td>
                                     <td className='py-2 border border-white text-center'>
                                         <div className='flex items-center justify-center '>
-                                            <span>{item.password}</span>
+                                            <span>{"*".repeat(item.password.length)}</span>
                                             <div className='lordiconcopy size-7 cursor-pointer' onClick={() => { copyText(item.password) }}>
                                                 <lord-icon
                                                     style={{ "width": "25px", "height": "25px", "paddingTop": "3px", "paddingLeft": "3px" }}
@@ -223,23 +196,22 @@ const Manager = () => {
                                         </div>
                                     </td>
                                     <td className='justify-center py-2 border border-white text-center'>
-                                        <span className='cursor-pointer mx-1' onClick={()=>{editPassword(item.id)}}>
+                                        <span className='cursor-pointer mx-1' onClick={() => { editPassword(item.id) }}>
                                             <lord-icon
                                                 src="https://cdn.lordicon.com/gwlusjdu.json"
                                                 trigger="hover"
-                                                style={{"width":"25px", "height":"25px"}}>
+                                                style={{ "width": "25px", "height": "25px" }}>
                                             </lord-icon>
                                         </span>
-                                        <span className='cursor-pointer mx-1'onClick={()=>{deletePassword(item.id)}}>
+                                        <span className='cursor-pointer mx-1' onClick={() => { deletePassword(item.id) }}>
                                             <lord-icon
                                                 src="https://cdn.lordicon.com/skkahier.json"
                                                 trigger="hover"
-                                                style={{"width":"25px", "height":"25px"}}>
+                                                style={{ "width": "25px", "height": "25px" }}>
                                             </lord-icon>
                                         </span>
                                     </td>
                                 </tr>
-
                             })}
                         </tbody>
                     </table>}
